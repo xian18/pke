fc4edc3c8fab904cddf759ec2d5a558daccc6fad
pengping <1758328860@qq.com>
2020-06-07 11:00:27 +0800

    -T 修改了lab的grade脚本

--- gradelib.py
+++ gradelib.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import os, re, traceback
+import sys, os, re, traceback
 
 
 
@@ -9,7 +9,7 @@ def test_app(app):
     cmd = 'spike build/pk app/' + app+'>> pke_out.txt'
     os.system(cmd)
 
-def test(points, title=None, parent=None):
+def test(points, title=' ', parent=None):
     """Decorator for declaring test functions.  If title is None, the
     title of the test will be derived from the function name by
     stripping the leading "test_" and replacing underscores with
@@ -39,9 +39,26 @@ def test(points, title=None, parent=None):
 
 
 def show_grade():
+    print()
     print("Score: %d/%d" % (TOTAL, POSSIBLE))
 
 COLORS = {"default": "\033[0m", "red": "\033[31m", "green": "\033[32m"}
 
 def color(name, text):
     return COLORS[name] + text + COLORS["default"]
+
+class Runner():
+    pke_out = ""
+    def run_app(self, app):
+        cmd = 'riscv64-unknown-elf-gcc -o app/' + app +' app/'+ app + '.c'
+        ret = os.system(cmd)
+        if ret != 0:
+            print(color('red', 'running app error!'))
+            sys.exit(1)
+        cmd = 'spike build/pk app/' + app+'> pke_out.txt'
+        os.system(cmd)
+        self.get_pke_out()
+        
+    def get_pke_out(self):
+        with open("pke_out.txt", "r") as f:  
+            self.pke_out = f.read()
