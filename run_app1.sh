524b6e11ccb609352da97cde731396e8c4f12c17
pengping <1758328860@qq.com>
2020-06-30 10:18:43 +0800

    删除了没用的文件

--- run_app1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-riscv64-unknown-elf-gcc -o app/elf/app1 app/app1.c
-spike build/pk app/elf/app1
--- run_app2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-riscv64-unknown-elf-gcc -o app/elf/app2 app/app2.c
-spike build/elf/pk app/app2
